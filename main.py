8888d03f4f6f09d943e5bbe07c281e3e3b492c13
Nery Estudillo <Estudner000@warren.k12.12.in.us>
2024-11-01 10:10:22 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#SECTION 1
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+
+print(fruit_tuple[-1])
+slice_tuple = [1,2 + 1]
+print(slice_tuple[:3])
+
+#SECTION 2
+num_tuple = (1, 2, 2, 3, 4)
+print(num_tuple.count(2))
+print(num_tuple.index(3))
+
+
+
+#SECTION 3
+my_tuple = "person info", 2 + 1, True
+person_info, number, boolean = my_tuple
+print(person_info)
+print(number)
+print(boolean)
+
